66b4435ec727c32aa7cb4ad6f580560000e25988
K Walls <65844397+kwalls13@users.noreply.github.com>
2020-08-30 17:46:13 +0800

    Add files via upload

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,22 +2,19 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({ robots }) => {
-  return (
-    <div>
-      {
-        robots.map((user, i) => {
-          return (
-            <Card
-              key={i}
-              id={robots[i].id}
-              name={robots[i].name}
-              email={robots[i].email}
-              />
-          );
-        })
-      }
-    </div>
+	return(
+  <React.StrictMode>
+		{robots.map((user, i) => {
+		return (
+			<Card 
+				key={i} 
+				id={robots[i].id} 
+				name={robots[i].name} 
+				email={robots[i].email}/>
+				);
+	})}
+  </React.StrictMode>
   );
 }
 
-export default CardList;
+export default CardList; 
